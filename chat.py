a273c8a4e76151fc0268af7bcabe177167fe3b87
Timyphey <timmelgamer@gmail.com>
2025-04-02 22:49:32 +0200

    Refactor source display logic into a separate function for improved readability and maintainability

--- chat.py
+++ chat.py
@@ -2,6 +2,12 @@ import streamlit as st
 import time
 from graph import invoke_user_question
 
+def display_sources_in_expander(sources, content_container):
+    with content_container.expander("Quellen"):
+        for i, (doc, similarity_score) in enumerate(sources):
+            with st.popover(f"Datei: {i + 1}: {doc.metadata.get('source', 'Unknown File')} - Seite: {doc.metadata.get('page_label', 'Unknown Page')} - Score: {similarity_score:.2f}"):
+                st.markdown(doc.page_content)
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Let's start chatting!"}]
@@ -11,10 +17,8 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
         if message["role"] == "assistant" and "source" in message:
-            st.subheader("Quellen")
-            for i, (doc, similarity_score) in enumerate(message["source"]):
-                with st.expander(f"Datei: {i + 1}: {doc.metadata.get('source', 'Unknown File')} - Seite: {doc.metadata.get('page_label', 'Unknown Page')} - Score: {similarity_score:.2f}"):
-                    st.markdown(doc.page_content)
+            sources_container = st.empty()
+            display_sources_in_expander(message["source"], sources_container)
 
 # Accept user input
 if prompt := st.chat_input("Ich hab da mal ne Frage..."):
@@ -32,11 +36,14 @@ if prompt := st.chat_input("Ich hab da mal ne Frage..."):
         graph_response = invoke_user_question(prompt)
         
         response.markdown(graph_response["answer"])  
-        st.subheader("Quellen")  
-        for i, (doc, similarity_score) in enumerate(graph_response["context"]):
-            with st.expander(f"Datei: {i + 1}: {doc.metadata.get('source', 'Unknown File')} - Seite {doc.metadata.get('page_label', 'Unknown Page')} - Score: {similarity_score:.2f}"):
-                st.markdown(doc.page_content)
+        
+        message = {"role": "assistant", "content": graph_response["answer"], "source": graph_response["context"]}
+        
+        sources_container = st.empty()
+        display_sources_in_expander(message["source"], sources_container)
+                    
+        # Add assistant response to chat history
+        st.session_state.messages.append(message)
         
         
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": graph_response["answer"], "source": graph_response["context"]})
+    
